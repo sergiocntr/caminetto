2092f945b784b06f13bbf388ffb4fdcc9d9b5bd1
sergio contro <sergiocntr@gmail.com>
2022-10-29 16:37:23 +0200

    interfaccia node red x controllo PID

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,27 @@ void setup(){
   pinMode(LED_BUILTIN,OUTPUT);
   digitalWrite(LED_BUILTIN,HIGH);
   wifiK::wifiOFF();  //spengo il wifi all'avvio
-  //Serial.begin(9600);
+  Serial.begin(9600);
   cs = K_ALL_OK;
-  wifiK::blinkLed(cs);
-  delay(2000);
+  DEBUG_PRINT(msg[cs]);
+  delay(500);
+  eeK::read();
+  delay(50);
+  DEBUG_PRINT(msg[cs]);
   if(cs == K_EP_READ_FAIL) {
   
     settaggi.sMaxFan = 255;   //settaggi di default se non viene letta correttamente la eeprom 
     settaggi.sMinFan = 100;
     settaggi.sTemp = 10;
-    settaggi.sOffset = 1.2;
     delay(100);
     eeK::write();
-   delay(100);
+    DEBUG_PRINT(msg[cs]);
   }
-  wifiK::blinkLed(cs);
-  delay(2000);
-  sondaK::offset = settaggi.sOffset;     //impostazione offset termocoppia
+  
   sondaK::setupK();                     //configurazione scheda SPI termocoppia
   delay(100);
+  
+  wifiK::blinkLed(cs);
   sondaK::getTemperature();
   delay(100);
   //Serial.println("0");
@@ -42,85 +44,77 @@ void setup(){
   wifiK::setupWifi();  //setup Wifi
   delay(100);
   wifiK::connWifi();
-  //Serial.println("3");
   delay(100);
+  DEBUG_PRINT(msg[cs]);
+  delay(30);
   wifiK::startMqtt();
-  //Serial.println("4");
- 
-  sondaDS::setupDS();  //setup sonda DS18b20
-  wifiK::smartDelay(100);
-  
-  scPID::Setpoint = settaggi.sTemp;  //set point della ventola (il pid la farà piu veloce tanto la temperatura è piu alta )
-
-  wifiK::smartDelay(100);
-  scPID::setupPid();  //setup PID
+  DEBUG_PRINT(msg[cs]);
   wifiK::smartDelay(100);
   wifiK::sendSetUp();
   wifiK::smartDelay(100);
+  DEBUG_PRINT("sendST");
   wifiK::sendStato();
 }
+void checkStato(){
+if(cs == K_SLEEP_REQUEST){ 
+  wifiK::sendStato();
+  wifiK::adessoDormo();
+}
+
+if(cs == K_EE_WRITE_REQ) {
+  eeK::write();
+  wifiK::smartDelay(100);
+  DEBUG_PRINT(msg[cs]);
+  wifiK::sendStato();
+
+  wifiK::smartDelay(100);
+  
+  eeK::read();
+  DEBUG_PRINT(msg[cs]);
+  wifiK::smartDelay(100);
+  wifiK::sendStato();
+
+  wifiK::smartDelay(100);
+  wifiK::sendSetUp();
+
+  cs = K_REBOOT_REQUEST;
+}
+}
 void loop(){
-  if(cs == K_REBOOT_REQUEST) setup();
+  
+  
   float lettura = 0.0;
-  //float medianK = 0.0;
-  //float medianDS = 0.0;
-   for (size_t i = 0; i < 5; i++)
+  for (size_t i = 0; i < 5; i++)
    {
     lettura = sondaK::getTemperature();
-    delay(100);
     wifiK::t_sondaK = medianFilterK.AddValue(lettura);
     wifiK::smartDelay(100);
- //   Serial.print("K is ");
-	//	Serial.println(lettura);
- //   Serial.print("K median is ");
-//		Serial.println(wifiK::t_sondaK);
-    //delay(100);
+
     lettura = sondaDS::getTemperature();
-    delay(100);
     wifiK::t_sondaDS = medianFilterDS.AddValue(lettura);
+    wifiK::smartDelay(100);
+
     scPID::pidloop(wifiK::t_sondaDS);
-    //wifiK::smartDelay(100);
-   // Serial.print("DS is ");
-	//	Serial.println(lettura);
-   // Serial.print("DS median is ");
-	//	Serial.println(wifiK::t_sondaDS);
+
+    checkStato();
+    if(cs == K_REBOOT_REQUEST) setup();
+    
     wifiK::smartDelay(6000);
    }
  
+ 
  if (sondaK::errNr > 0)
-  {
-    wifiK::blinkLed(1);
-    delay(250);
-    wifiK::sendAlarm(sondaK::errNr);
-    
-  }
-
-
-delay(10);
+{
+  wifiK::blinkLed(1);
+  delay(250);
+  wifiK::sendAlarm(sondaK::errNr);
+  
+}
+wifiK::smartDelay(10);
 wifiK::OutPutPID = scPID::_Output;
-  //if(avg< 35) return;
- // wifiK::t_sondaK =medianK; 
-    // Serial.print("K media ");
-		// Serial.println(wifiK::t_sondaK);
-  //wifiK::t_sondaDS  =medianDS ;
-     // Serial.print("DS media ");
-	//	Serial.println(wifiK::t_sondaDS);
-  delay(10);
-  wifiK::sendThing();
-  wifiK::smartDelay(100);
-  if(cs == K_SLEEP_REQUEST){ 
-    wifiK::sendStato();
-    wifiK::adessoDormo();
-  }
-  if(cs == K_EE_WRITE_REQ) {
-    eeK::write();
-    delay(100);
-    wifiK::sendStato();
-    delay(100);
-    eeK::read();
-    delay(100);
-    wifiK::sendStato();
-    delay(100);
-  }
+
+wifiK::smartDelay(10);
+wifiK::sendThing();
+wifiK::smartDelay(100);
 
 }
